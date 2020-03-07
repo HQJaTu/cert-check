3aa918ebd34b3becbb37c64e85d19d5ee5775d70
Jari Turkia <jmjt@jari.turkia.name>
2020-03-07 11:23:04 +0200

    Bumped version into 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cert-check',
-    version='0.0.1',
+    version='0.1.0',
     url='https://github.com/HQJaTu/cert-check',
     license='GPLv2',
     author='Jari Turkia',
