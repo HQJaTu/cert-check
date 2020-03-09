7d81a85fa1a9cf8a6bf63683c4ca2d1b0f25e707
Jari Turkia <jmjt@jari.turkia.name>
2020-03-09 21:35:32 +0200

    Bugfix: Yet another type of HTTP-status messages from Let's Encrypt OCSP-server added

--- cert_check_lib/ocsp_check.py
+++ cert_check_lib/ocsp_check.py
@@ -83,7 +83,7 @@ class OcspChecker:
                 time.sleep(5)
                 continue
             except requests_exceptions.HTTPError as exc:
-                if exc.response.status_code not in [404, 500, 502]:
+                if exc.response.status_code not in [404, 500, 502, 504]:
                     raise
 
                 # Go another round after bit of a cooldown
