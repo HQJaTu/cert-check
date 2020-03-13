dc7a1b6c03ae19de5995266d89a2ef65a45491fe
Jari Turkia <jmjt@jari.turkia.name>
2020-03-14 00:45:19 +0200

    Bugfix: Enforce HTTP and HTTPS -schemes for getting issuer certs

--- cert_check_lib/requests.py
+++ cert_check_lib/requests.py
@@ -1,5 +1,6 @@
 import aiohttp
 import asyncio
+from urllib.parse import urlparse
 
 
 class RequestsSession:
@@ -13,24 +14,32 @@ class RequestsSession:
 
     @staticmethod
     async def get_issuer_cert_async(loop, ca_issuer_url, timeout):
+        o = urlparse(ca_issuer_url)
+        if not o.scheme in ['http', 'https']:
+            return None, None
+		
         try:
             async with RequestsSession.get_requests_retry_session(retries=2, loop=loop) as session:
                 async with session.get(ca_issuer_url) as response:
                     return response, await response.read()
-        except aiohttp.ClientError:
+        except (aiohttp.ClientError, aiohttp.ClientConnectorError):
             return None, None
         except asyncio.TimeoutError:
             return None, None
 
     @staticmethod
     async def post_ocsp_request_async(loop, url, headers, ocsp_request, timeout):
+        o = urlparse(url)
+        if not o.scheme in ['http', 'https']:
+            return False, None, None
+		
         response = None
         should_retry = None
         async with RequestsSession.get_requests_retry_session(retries=2, loop=loop) as session:
             try:
                 async with session.post(url, headers=headers, data=ocsp_request, timeout=timeout) as response:
                     return False, response, await response.read()
-            except aiohttp.ClientError as exc:
+            except (aiohttp.ClientError, aiohttp.ClientConnectorError) as exc:
                 should_retry = False
                 await session.close()
             except asyncio.TimeoutError as exc:
